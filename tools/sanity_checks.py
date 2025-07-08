f420f12156a85efed4282205b43494c45c13e1f7
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2025-07-07 19:33:34 -0500

    cfitsio v4.6.2

--- tools/sanity_checks.py
+++ tools/sanity_checks.py
@@ -40,6 +40,9 @@ PER_PROJECT_PERMITTED_FILES = {
     'bzip2': [
         'test.py',
     ],
+    'cfitsio': [
+        'test_compare.py',
+    ],
     'curl': [
         'buildinfo.txt.meson',
         'extract.mk',
